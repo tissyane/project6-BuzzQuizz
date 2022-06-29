8d5e2af8fd7cbd42d8f8272c00a5b5befcc53bf5
rodrigocqb <rodrigocortibarros@gmail.com>
2022-06-29 14:34:51 -0300

    Quizzes sendo adicionados, faltando formatação

---
+++ scripts/list_all_quizzes.js
@@ -0,0 +1,16 @@
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+promise.then(escreverQuizzes);
+
+//essa função vai receber a lista e passar ela para o HTML
+let quizzlist;
+function escreverQuizzes(list) {
+    quizzlist = list.data;
+    const QuizzBoard = document.querySelector(".all-quizzes");
+    for (let i = 0; i < quizzlist.length; i++){
+        QuizzBoard.innerHTML +=`
+        <div class ="quizz">
+            <img class="quizz-img" src="${quizzlist[i].image}" />
+            <p>${quizzlist[i].title}</p>
+        </div>`
+    }
+}
