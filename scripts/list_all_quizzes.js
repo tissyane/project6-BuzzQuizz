d704dc5ebb2ee7a76e3462f1ac510be81f612490
rodrigocqb <rodrigocortibarros@gmail.com>
2022-06-29 17:19:36 -0300

    Aplicando degradê - style direto no html

--- scripts/list_all_quizzes.js
+++ scripts/list_all_quizzes.js
@@ -8,7 +8,7 @@ function escreverQuizzes(list) {
     const QuizzBoard = document.querySelector(".all-quizzes");
     for (let i = 0; i < quizzlist.length; i++){
         QuizzBoard.innerHTML +=`
-        <div class ="quizz" style="background-image: url('${quizzlist[i].image}');">
+        <div class ="quizz" style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url('${quizzlist[i].image}');">
             <p>${quizzlist[i].title}</p>
         </div>`
     }
