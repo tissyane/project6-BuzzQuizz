0038e5f30e6f430038a9f6ceee76dbd423fdbc54
rodrigocqb <rodrigocortibarros@gmail.com>
2022-06-30 22:26:51 -0300

    Layout inserção de respostas pronto

--- scripts/create_quizz.js
+++ scripts/create_quizz.js
@@ -11,7 +11,7 @@ function createQuizz(){
     main.innerHTML =`
     <div class="create">
         <h2>Comece pelo começo</h2>
-        <div class="informacoes-quizz">
+        <div class="quizz-info">
             <div>
                 <input class="create-title" placeholder="Título do seu quizz"></input>
                 <input class="create-image" placeholder="URL da imagem do seu quizz"></input>
@@ -40,25 +40,55 @@ function createQuizz2(){
         main.innerHTML =`
         <div class="create">
             <h2>Crie suas perguntas</h2>
-            <div class="perguntas-quizz">
+            <div class="quizz-questions">
                 
             </div>
             <button onclick="createQuizz3()">Prosseguir para criar níveis</button>
         </div>`;
         
-        const perguntas = document.querySelector(".perguntas-quizz");
+        const questions = document.querySelector(".quizz-questions");
         for(let i = 1; i <= NumberOfQuestions; i++){
-            perguntas.innerHTML += `
-            <div class="pergunta">
-                <div>
+            questions.innerHTML += `
+            <div class="question">
+                <div class="question-click">
                     <h3>Pergunta ${i}</h3>
-                    <img onclick="insertQuestions()" src="images/create.svg" />
-                </div>    
+                    <img onclick="insertQuestions(this)" src="images/create.svg" />
+                </div>
+                <div class="question-open hidden">
+                    <div class="question-data">
+                        <h3>Pergunta ${i}</h3>
+                        <input placeholder="Texto da pergunta"></input>
+                        <input placeholder="Cor de fundo da pergunta"></input>
+                    </div>
+                    <div class="question-data">
+                        <h3>Resposta correta</h3>
+                        <input placeholder="Resposta correta"></input>
+                        <input placeholder="URL da imagem"></input>
+                    </div>
+                    <div class="question-data">
+                        <h3>Respostas incorretas</h3>
+                        <div>
+                            <input placeholder="Resposta incorreta 1"></input>
+                            <input placeholder="URL da imagem 1"></input>
+                        </div>
+                        <div>
+                            <input placeholder="Resposta incorreta 2"></input>
+                            <input placeholder="URL da imagem 2"></input>
+                        </div>
+                        <div>
+                            <input placeholder="Resposta incorreta 3"></input>
+                            <input placeholder="URL da imagem 3"></input>
+                        </div>
+                    </div>
+                </div>
             </div>`;
         }
     }
 }
 
-function insertQuestions(){
+function insertQuestions(question){
     //toggle hide nos icones e aparece as divs de input
+    question = question.parentNode.parentNode;
+    question.querySelector(".question-open").classList.remove("hidden");
+    question.querySelector(".question-click").classList.add("hidden");
 }
